63782bbddb5359a597aa2ee6b0d03859211cef56
itsshashanksp <9945shashank@gmail.com>
2026-07-10 13:29:31 +0530

    Improve responsive design for smaller screens

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,18 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 import "./styles/global.css";
+import "./styles/components.css";
+import "./styles/navbar.css";
+import "./styles/hero.css";
+import "./styles/about.css";
+import "./styles/services.css";
+import "./styles/products.css";
+import "./styles/stats.css";
+import "./styles/industries.css";
+import "./styles/locateus.css";
+import "./styles/enquiry.css";
+import "./styles/footer.css";
+import "./styles/responsive.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
